fc180a4ea3c430acefa56673e41f90b09cf82266
bryanarboledaa <jbdarboleda@gmail.com>
2023-07-01 19:08:37 +0800

    helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
 module UsersHelper
 
   def fetch_track_from_s3(key)
-    s3 = Aws::S3::Resource.new(region: 'us-west-2')
+    s3 = Aws::S3::Resource.new(access_key_id: 'AKIATINIPJ427TNA4QPB',
+                               secret_access_key: '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',
+                               region: 'us-west-2')
     bucket = s3.bucket('bryan-test-ftp')
     object = bucket.object(key)
 
